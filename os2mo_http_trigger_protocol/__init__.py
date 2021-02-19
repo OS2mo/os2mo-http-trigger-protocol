c871b0e9b49ba7a2b5c58af1ce23d2443def2b6a
Emil Madsen <sovende@gmail.com>
2021-02-19 16:18:17 +0100

    Bump os2mo-http-protocol version

--- os2mo_http_trigger_protocol/__init__.py
+++ os2mo_http_trigger_protocol/__init__.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 from enum import Enum, auto, unique
 
 from pydantic import BaseModel
@@ -11,31 +11,26 @@ class AutoNameEnum(Enum):
 
 
 @unique
-class EventType(AutoNameEnum):
+class EventType(str, AutoNameEnum):
     """MO Trigger EventType.
 
     EventType for trigger registry
     """
 
-    ON_BEFORE = auto()
-    ON_AFTER = auto()
+    ON_BEFORE: str = auto()
+    ON_AFTER: str = auto()
 
 
 @unique
-class RequestType(AutoNameEnum):
+class RequestType(str, AutoNameEnum):
     """MO Trigger RequestType.
 
     Support requests for :class:`RequestHandler`.
     """
 
-    CREATE = auto()
-    EDIT = auto()
-    TERMINATE = auto()
-
-
-class OUObject(BaseModel):
-    type: str
-    data: Dict
+    CREATE: str = auto()
+    EDIT: str = auto()
+    TERMINATE: str = auto()
 
 
 class MOTriggerPayload(BaseModel):
@@ -43,30 +38,22 @@ class MOTriggerPayload(BaseModel):
 
     See: https://os2mo.readthedocs.io/en/development/api/triggers.html#the-trigger-function for details.
 
-    Note: data is dependent on the `event_type, `request_type` and `role_type`.
+    Note: request is dependent on the `event_type, `request_type` and `role_type`.
     """
 
     class Config:
         schema_extra = {
             "example": {
-                "event_type": 0,
-                "request": {
-                    "data": {
-                        "clamp": True,
-                        "name": "Havtorn Kommune",
-                        "uuid": "fb2d158f-114e-5f67-8365-2c520cf10b58",
-                        "validity": {"from": "2021-02-09"},
-                    },
-                    "type": "org_unit",
-                },
-                "request_type": 1,
+                "event_type": "ON_BEFORE",
+                "request": {},
+                "request_type": "EDIT",
                 "role_type": "org_unit",
                 "uuid": "fb2d158f-114e-5f67-8365-2c520cf10b58",
             }
         }
 
     event_type: EventType
-    request: OUObject
+    request: Dict
     request_type: RequestType
     role_type: str
     uuid: UUID
