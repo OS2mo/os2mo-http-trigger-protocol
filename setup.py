d6aa4a8c62b4641089ca127a07a8e1ec3e297617
Emil Madsen <sovende@gmail.com>
2021-02-19 16:26:24 +0100

    Bump os2mo-http-protocol version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="os2mo-http-trigger-protocol",
-    version="0.0.1",
+    version="0.0.2",
     author="Magenta ApS",
     author_email="info@magenta.dk",
     description="Protocol library for OS2mo HTTP triggers",
